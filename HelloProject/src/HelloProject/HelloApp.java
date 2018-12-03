fcc794e4ad4233c20a8e429bef4248978c7c99a1
Ryan Robichaud <robichaud.ryan@gmail.com>
2018-12-03 14:45:11 -0400

    New printed text

--- HelloProject/src/HelloProject/HelloApp.java
+++ HelloProject/src/HelloProject/HelloApp.java
@@ -3,7 +3,7 @@ package HelloProject;
 public class HelloApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Ry!");
+		System.out.println("Hello Ryan Robichaud!");
 	}
 
 }
